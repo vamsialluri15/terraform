1f4768fb7ec53d1713709df318e5ca648ad90df1
alluri vamsi <112869290+vamsialluri15@users.noreply.github.com>
2023-02-18 14:43:44 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,6 +3,6 @@
 
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIA4BJYW4HJY2LPLOUM"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
